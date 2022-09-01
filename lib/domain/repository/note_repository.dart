5964a88f91899b24bac2e45bd10bc5f2f3937c29
wogks <wogks27@naver.com>
2022-09-01 17:05:10 +0900

    Feat: local db 작성

---
+++ lib/domain/repository/note_repository.dart
@@ -0,0 +1,13 @@
+import 'package:note_app/domain/model/my_note.dart';
+
+abstract class NoteRepository {
+  Future<List<MyNote>> getNotes();
+
+  Future<MyNote> getNoteById(int id);
+
+  Future<void> insertNote(MyNote note);
+
+  Future<void> updateNote(MyNote note);
+
+  Future<void> deleteNote(MyNote note);
+}
